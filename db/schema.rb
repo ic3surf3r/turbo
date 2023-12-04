610e66797b0e2224335c51daaac88641e76009f3
Maxime Neven <maxime.neven@gmail.com>
2023-12-04 16:07:24 +0100

    Location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_082547) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_101153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_082547) do
     t.bigint "club_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["club_id"], name: "index_locations_on_club_id"
   end
 
