85b9669ac994f01d01c763c6983870fe70e21780
Maxime Neven <maxime.neven@gmail.com>
2023-12-05 10:21:35 +0100

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,80 @@ User.destroy_all
 puts "Deleting clubs"
 Club.destroy_all
 
-
-
+# 18 users
 puts "Creating users"
-bob = User.create!(email: "bob@email.com", first_name: "Bob", last_name: "Smith", password: "123123")
-stacy = User.create!(email: "stacy@email.com", first_name: "Stacy", last_name: "Valentine", password: "123123")
-kevin = User.create!(email: "kevin@email.com", first_name: "Kevin", last_name: "Elevin", password: "123123")
+maxime = User.create!(email: "maxime@email.com", first_name: "Maxime", last_name: "Neven", password: "123123")
+hugo = User.create!(email: "hugo@email.com", first_name: "Hugo", last_name: "Benhaiem", password: "123123")
+quentin = User.create!(email: "quentin@email.com", first_name: "Quentin", last_name: "Noel", password: "123123")
+arnaud = User.create!(email: "arnaud@email.com", first_name: "Arnaud", last_name: "Dykmans", password: "123123")
+edgar = User.create!(email: "edgar@email.com", first_name: "Edgar", last_name: "Reynaud", password: "123123")
+loris = User.create!(email: "loris@email.com", first_name: "Loris", last_name: "Grandchamp", password: "123123")
+thibault = User.create!(email: "thibault@email.com", first_name: "Thibault", last_name: "Neven", password: "123123")
+franki = User.create!(email: "franki@email.com", first_name: "Franki", last_name: "Harte", password: "123123")
+rodolphe = User.create!(email: "rodolphe@email.com", first_name: "Rodolphe", last_name: "Willocx", password: "123123")
+bobbi = User.create!(email: "bobbi@email.com", first_name: "Bobbi", last_name: "Van Bogaert", password: "123123")
+cyril = User.create!(email: "cyril@email.com", first_name: "Cyril", last_name: "Hermans", password: "123123")
+corentin = User.create!(email: "corentin@email.com", first_name: "Corentin", last_name: "Desmet", password: "123123")
+marco = User.create!(email: "marco@email.com", first_name: "Marco", last_name: "Moretto", password: "123123")
+victor = User.create!(email: "victor@email.com", first_name: "Victor", last_name: "Saint-Martin", password: "123123")
+harald = User.create!(email: "harald@email.com", first_name: "Harald", last_name: "Marquet", password: "123123")
+greg = User.create!(email: "greg@email.com", first_name: "Greg", last_name: "Gucassof", password: "123123")
+edouard = User.create!(email: "edouard@email.com", first_name: "Edouard", last_name: "Vandenhaute", password: "123123")
+bastien = User.create!(email: "bastien@email.com", first_name: "Bastien", last_name: "Aerts", password: "123123")
+lionel = User.create!(email: "lionel@email.com", first_name: "Lionel", last_name: "Sampoux", password: "123123")
 
-puts "Creating White Starr"
-white_star = Club.create!(name: "Royal White Star A.C.", description: "Hockey team in first division", address: "Chemin du Struykbeken 2, 1200 Bruxelles")
+#Club White Star
+puts "Creating White Star"
+white_star = Club.create!(name: "Royal White Star Hockey Club", description: "Hockey club located in Evere", address: "Av. des Anciens Combattants 350, 1140 Evere")
 white_star.photo.attach(
   io: File.open('app/assets/images/white_star.gif'),
   filename: 'white_star.gif'
 )
-ClubMember.create!(club_id: white_star.id, user_id: bob.id)
-ClubMember.create!(club_id: white_star.id, user_id: stacy.id, is_manager: true)
-ClubMember.create!(club_id: white_star.id, user_id: kevin.id)
 
-first_team = Team.create!(club_id: white_star.id, name: "First team", sport: "Hockey")
+#18 Club Members
+ClubMember.create!(club_id: white_star.id, user_id: maxime.id)
+ClubMember.create!(club_id: white_star.id, user_id: arnaud.id)
+ClubMember.create!(club_id: white_star.id, user_id: edgar.id)
+ClubMember.create!(club_id: white_star.id, user_id: loris.id)
+ClubMember.create!(club_id: white_star.id, user_id: thibault.id)
+ClubMember.create!(club_id: white_star.id, user_id: franki.id)
+ClubMember.create!(club_id: white_star.id, user_id: rodolphe.id)
+ClubMember.create!(club_id: white_star.id, user_id: bobbi.id)
+ClubMember.create!(club_id: white_star.id, user_id: cyril.id)
+ClubMember.create!(club_id: white_star.id, user_id: corentin.id)
+ClubMember.create!(club_id: white_star.id, user_id: marco.id)
+ClubMember.create!(club_id: white_star.id, user_id: victor.id)
+ClubMember.create!(club_id: white_star.id, user_id: harald.id)
+ClubMember.create!(club_id: white_star.id, user_id: greg.id)
+ClubMember.create!(club_id: white_star.id, user_id: edouard.id)
+ClubMember.create!(club_id: white_star.id, user_id: bastien.id)
+ClubMember.create!(club_id: white_star.id, user_id: hugo.id)
+ClubMember.create!(club_id: white_star.id, user_id: lionel.id)
+ClubMember.create!(club_id: white_star.id, user_id: quentin.id, is_manager: true)
+
+first_team = Team.create!(club_id: white_star.id, name: "Men's first team", sport: "Hockey")
 TeamMember.create!(team_id: first_team.id, user_id: stacy.id)
-TeamMember.create!(team_id: first_team.id, user_id: bob.id)
-TeamMember.create!(team_id: first_team.id, user_id: kevin.id, is_coach: true)
+TeamMember.create!(team_id: white_star.id, user_id: maxime.id)
+TeamMember.create!(team_id: white_star.id, user_id: arnaud.id)
+TeamMember.create!(team_id: white_star.id, user_id: edgar.id)
+TeamMember.create!(team_id: white_star.id, user_id: loris.id)
+TeamMember.create!(team_id: white_star.id, user_id: thibault.id)
+TeamMember.create!(team_id: white_star.id, user_id: franki.id)
+TeamMember.create!(team_id: white_star.id, user_id: rodolphe.id)
+TeamMember.create!(team_id: white_star.id, user_id: bobbi.id)
+TeamMember.create!(team_id: white_star.id, user_id: cyril.id)
+TeamMember.create!(team_id: white_star.id, user_id: corentin.id)
+TeamMember.create!(team_id: white_star.id, user_id: marco.id)
+TeamMember.create!(team_id: white_star.id, user_id: victor.id)
+TeamMember.create!(team_id: white_star.id, user_id: harald.id)
+TeamMember.create!(team_id: white_star.id, user_id: greg.id)
+TeamMember.create!(team_id: white_star.id, user_id: edouard.id)
+TeamMember.create!(team_id: white_star.id, user_id: bastien.id)
+TeamMember.create!(team_id: white_star.id, user_id: hugo.id, is_coach: true)
 
-senior_team = Team.create!(club_id: white_star.id, name: "Senior team", sport: "Hockey")
-TeamMember.create!(team_id: senior_team.id, user_id: kevin.id, is_coach: true)
 
+senior_team = Team.create!(club_id: white_star.id, name: "Women's first team", sport: "Hockey")
+TeamMember.create!(team_id: senior_team.id, user_id: lionel.id, is_coach: true)
 
 30.times do
   first_name = Faker::Name.first_name
